4f4200130932cb64b94d6e4c1e6db62848f3740d
niyanli <39321830+niyanli@users.noreply.github.com>
2018-05-22 15:03:07 +0800

    Update css3tutorial.js

--- html5andcss3/css3/js/css3tutorial.js
+++ html5andcss3/css3/js/css3tutorial.js
@@ -10,4 +10,11 @@
 			$('#sidebar_wrapper').removeClass('toggled');
 		}			
 	});
+
+	hljs.initHighlightingOnLoad();
+	$(document).ready(function() {
+	  $('pre code').each(function(i, block) {
+	    hljs.highlightBlock(block);
+	  });
+	});
 })();
